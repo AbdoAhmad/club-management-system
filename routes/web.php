b890ca8c97f50561d8a6ddc5c60c01eba148fbaf
AbdoAhmad <36090818+AbdoAhmad@users.noreply.github.com>
2026-04-23 12:14:09 +0200

    feat: implement multi-tenancy using stancl/tenancy with database and domain separation

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
-use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
-
+use App\Livewire\Home;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 // routes/web.php
-
-Route::group(['prefix' => LaravelLocalization::setLocale(),
-    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
-], function () {
-    /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
-    Route::get('/', Home::class)->name('home');
-});
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        // your actual routes
+        Route::group(['prefix' => LaravelLocalization::setLocale(),
+            'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
+        ], function () {
+            /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+            Route::get('/', Home::class)->name('home');
+        });
+    });
+}
