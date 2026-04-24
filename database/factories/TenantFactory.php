00bad32c4239192557103e4439fd0912cfaec78f
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2026-04-24 17:44:22 +0300

    firs

--- database/factories/TenantFactory.php
+++ database/factories/TenantFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Model;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends Factory<Model>
@@ -33,6 +34,13 @@ class TenantFactory extends Factory
             $tenant->domains()->create([
                 'domain' => $tenant->id . '.localhost',
             ]);
+            tenancy()->initialize($tenant->id);
+            User::factory()->create([
+                'name' => 'Test Admin',
+                'email' => 'test@example.com',
+            ]);
+            tenancy()->end();
+
         });
     }
 }
