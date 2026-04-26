9ebdc267ea7dbeebd7f4a85b171561f250252b33
AbdoAhmad <36090818+AbdoAhmad@users.noreply.github.com>
2026-04-26 09:53:21 +0300

    feat: implement Player module with database schema, factory, seeder, and initial Livewire components

---
+++ app/Livewire/PlayerManagment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PlayerManagment extends Component
+{
+    public function render()
+    {
+        return view('livewire.player-managment');
+    }
+}
