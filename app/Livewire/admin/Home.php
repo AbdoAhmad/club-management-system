146239e96db452833c7ea09d17ca926f10c86f91
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2026-04-27 17:21:15 +0300

    improve

--- app/Livewire/admin/Home.php
+++ app/Livewire/admin/Home.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire\admin;
 
+use App\Models\Tenant;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
-        return view('livewire.home');
+        $totalTenants = Tenant::count();
+        $activeTenants = Tenant::where('status', 'active')->count();
+        $archivedTenants = Tenant::onlyTrashed()->count();
+        $recentTenants = Tenant::latest()->take(5)->get();
+
+        return view('livewire.home', [
+            'totalTenants' => $totalTenants,
+            'activeTenants' => $activeTenants,
+            'archivedTenants' => $archivedTenants,
+            'recentTenants' => $recentTenants,
+        ]);
     }
 }
